a30c78fc3dc7fe73e5513a2c049b3d25f3205e25
Lazaro O'Farrill <lazaroofarrill@gmail.com>
2025-07-18 21:07:32 +0200

    changes

--- ugee/m908/default.nix
+++ ugee/m908/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, qt5, kdePackages }:
+{ stdenv, qt5, libsForQt5 }:
 stdenv.mkDerivation {
   pname = "m908";
   version = "4.3.4";
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
       "https://ugeecom-download.oss-us-west-1.aliyuncs.com/file/2024/12/ugeeTablet-4.3.4-241031.tar.gz";
   };
 
-  nativeBuildInputs = [ kdePackages.wrapQtAppsHook ];
+  nativeBuildInputs = [ libsForQt5.qt5.wrapQtAppsHook ];
   buildInputs = [ qt5.qtbase ];
 
   installPhase = builtins.readFile ./install-script.sh;
